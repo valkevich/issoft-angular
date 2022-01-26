6a777a52f7299e9c6e63cb079b89278805684ce7
Nikita Valkevich <NikitaValkevich@coherentsolutions.com>
2022-01-26 11:52:19 +0300

    ready task-1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,59 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+
+
 import { AppComponent } from './app.component';
+import { FirstPageComponent } from './pages/first-page/first-page.component';
+import { SecondPageComponent } from './pages/second-page/second-page.component';
+import { ThirdPageComponent } from './pages/third-page/third-page.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CardComponent } from './components/card/card.component';
+import { ExpansionPanelComponent } from './components/expansion-panel/expansion-panel.component';
+import { SpinnerComponent } from './components/spinner/spinner.component';
+import { RadioButtonComponent } from './components/radio-button/radio-button.component';
+import { ButtonsComponent } from './components/buttons/buttons.component';
+import { ListComponent } from './components/list/list.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstPageComponent,
+    SecondPageComponent,
+    ThirdPageComponent,
+    NavbarComponent,
+    CardComponent,
+    ExpansionPanelComponent,
+    SpinnerComponent,
+    RadioButtonComponent,
+    ButtonsComponent,
+    ListComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatTabsModule,
+    MatChipsModule,
+    MatExpansionModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatButtonModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
